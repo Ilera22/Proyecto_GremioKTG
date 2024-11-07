77ce62b351a822d6221b3aa0023cb0797a20367e
Ilera22 <108095478+Ilera22@users.noreply.github.com>
2024-11-07 17:25:30 -0500

    Nuevo navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,36 +1,57 @@
 <template>
   <v-card>
     <v-layout>
-      <v-navigation-drawer expand-on-hover rail>
-        <v-list>
-          <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            subtitle="sandra_a88@gmailcom"
-            title="Sandra Adams"
-          ></v-list-item>
-        </v-list>
+      <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        @click="rail = false"
+      >
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          title="John Leider"
+          nav
+        >
+          <template v-slot:append>
+            <v-btn
+              icon="mdi-chevron-left"
+              variant="text"
+              @click.stop="rail = !rail"
+            ></v-btn>
+          </template>
+        </v-list-item>
 
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
           <v-list-item
-            prepend-icon="mdi-folder"
-            title="My Files"
-            value="myfiles"
+            prepend-icon="mdi-home-city"
+            title="Home"
+            value="home"
           ></v-list-item>
           <v-list-item
-            prepend-icon="mdi-account-multiple"
-            title="Shared with me"
-            value="shared"
+            prepend-icon="mdi-account"
+            title="My Account"
+            value="account"
           ></v-list-item>
           <v-list-item
-            prepend-icon="mdi-star"
-            title="Starred"
-            value="starred"
+            prepend-icon="mdi-account-group-outline"
+            title="Users"
+            value="users"
           ></v-list-item>
         </v-list>
       </v-navigation-drawer>
-
     </v-layout>
   </v-card>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        drawer: true,
+        rail: true,
+      }
+    },
+  }
+</script>
